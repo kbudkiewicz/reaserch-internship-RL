eeb31da3b3bf7ca136434be3fc4c05182eb5b6af
kbudkiewicz <krystianbudkiewicz@gmail.com>
2022-10-04 12:26:44 +0200

    My first commit to a project on GitHub!

---
+++ LunarLander(DQN).py
@@ -0,0 +1,117 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as tfunc
+import math
+import matplotlib.pyplot as mplt
+import numpy
+import gym
+import Box2D
+
+### Assigning the device
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+print("Current device: %s \n" % device)
+
+### Creating gym' Lunar Lander environment
+env = gym.make("LunarLander-v2", new_step_api=True)
+total_reward = 0.0
+total_steps = 0
+obs = env.reset()
+print(env.step(1))
+
+### Loop testing whether a condition in the environment (both legs touching the ground // True) is met. Then break the loop and return total score
+while True:
+    action = env.action_space.sample()
+    obs, reward, f1 ,f2, _ = env.step(action)
+    total_reward += reward
+    total_steps += 1
+    if f1 and f2 == True:
+        break
+print("Episode done in %d steps. Total score: %s" % (total_steps,total_reward))
+
+# nn.BatchNorm2d()
+
+class DQN(nn.Module):
+    def __init__(self):                                 # CNN not needed for research internship
+        super(DQN,self).__init__()                      # super
+        self.conv1 = nn.Linear(8,32)     # input (here 8) corresponds to the size of observation space
+        self.conv2 = nn.Linear(32,32)
+        self.conv3 = nn.Linear(32,4)     # output (here 4) corresponds to the size of action space
+
+        # stide         gives how much the filter is moved across the matrix (e.g. stride = 2 means: move the filter 2 indicies to the right of the matrix)
+        # kernel_size   size of the tensor/matrix filter between convolutional layers
+
+    def forward(self,state):
+        x = tfunc.relu(self.conv1(state))               # ReLU - rectified linear unit. take max(0,input) of the input
+        x = tfunc.relu(self.conv2(x))
+        return tfunc.relu(self.conv3(x))
+
+class Environment():
+    def __init__(self):
+        self.steps_left = 100
+
+    def get_obs(self):
+        return env.reset()
+
+    def get_action(self):
+        return env.action_space.sample()
+
+    def is_done(self):
+        return self.steps_left == 0
+
+    def action(self, action):
+        if self.is_done():
+            return "Testing is over"
+        self.steps_left -= 1
+        return numpy.random.random()
+
+class Agent():
+    def __init__(self):
+        self.score = 0.0
+
+    def step(self, env):
+        current_obs = env.observation_space().sample()
+        actions = env.action_space().sample()
+        reward = env.action(numpy.random.choice(actions))
+        self.score += reward
+
+### Training
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
+
+# def select_action(state):
+    # sample = random.random()
+    # eps_treshold = EPS_END + (EPS_START-EPS_END)* \ math.exp(-1. * steps_done / EPS_DECAY)
+    # steps_done += 1
+    # if sample > eps_treshold:
+    #     return policy_net(state).max(1)[1].view(1,1)
+    # else:
+    #     return torch.tensor([[random.randrange(n_actions)]], device=device,dtype=torch.long)
+
+### Training loop
+# episode_n = 10000
+# scores = []
+# for i_episode in range(n_episodes):
+#     state = env.reset()
+#     score = 0
+#     for z in range(max_t):
+#         action = agent.act(state,eps)
+#         next_state, reward, done, _ = env.step(action)
+#         agent.step(step, action, reward, next_state, done)
+    #     state = next_state
+#         score += reward
+    #     if done:
+#             break
+#     scores.append(score)
+#     if np.mean(scores) >= 200.00:
+#       print("Environment solved in %s iterations with avarage score %s" % (i_episode, np.mean(scores))
+#   return scores
+
+# env.render()
+# env.close()
---
+++ TESTING_PyTorch.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import gym
+
+class box():
+    def __init__(self,dimensions):
+        self.dim = dimensions
+
+k = box(24)
+print(k.dim)
+
+print(2//3,4//3)
+
+m = nn.Softmax(dim=1)
+input = torch.randn(2,3)
+print(input, m(input), sep="\n")
+
+### Environment of the Lunar Lander
+env = gym.make("LunarLander-v2", new_step_api=True)
+obs = env.reset()
+print(obs, "\n")
+print(env.action_space)
+action = env.action_space.sample()
+print(action, "\n")
+print(env.observation_space)
+print(env.step(0))
