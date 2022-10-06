c55b70a0c055afcc4b776cfd23c18147d8f44f48
kbudkiewicz <krystianbudkiewicz@gmail.com>
2022-10-06 13:57:50 +0200

    Renaming variables + new tests of PyTorch/Gym

--- LunarLander(DQN).py
+++ LunarLander(DQN).py
@@ -19,7 +19,6 @@ print("Current device: %s \n" % device.upper())
 
 ### Creating gym' Lunar Lander environment
 env = gym.make("LunarLander-v2")
-env.reset(seed=0)
 # obs = env.reset()
 # print(obs,'\n', env.step(1))
 
@@ -27,7 +26,7 @@ class DNNetwork(nn.Module):
     def __init__(self,layer_size=64):                   # CNN not needed for research internship -> Linear layers, batchnormalisation not needed
         super(DNNetwork,self).__init__()                # super(superclass) - inherit the methods of the superclass (class above this one). Here: inherit all __init__ method of DQN
         self.lin1 = nn.Linear(8,layer_size)             # input (here 8) corresponds to the size of observation space
-        self.lin2 = nn.Linear(layer_size,layer_size)
+        self.lin2 = nn.Linear(layer_size,layer_size)    # layer_size = amount of neurons between hidden layers
         self.lin3 = nn.Linear(layer_size,4)             # output (here 4) corresponds to the size of action space
 
         ### For CNNs
@@ -38,19 +37,21 @@ class DNNetwork(nn.Module):
         # x = state.to(torch.device)
         x = tfunc.relu( self.lin1(state) )               # ReLU - rectified linear unit. take max(0,input) of the input
         x = tfunc.relu( self.lin2(x) )
-        return tfunc.relu( self.lin3(x) )
+        return self.lin3(x)
 
 ### Defining replay memory
-transit = namedtuple( 'Transition',('s','a','r','s_next') )
+memory = namedtuple( 'Memory',('s','a','r','s_next') )
 
 class Replay_memory(object):
     def __init__(self,memory_size, batch_size):
         self.memory = deque(maxlen=memory_size)
         self.batch_size = batch_size
-    def push(self,*args):
-        self.memory.append( transit(*args) )
-    def sample(self):
+    def remember(self, *args):                           # *args: put any amount of arguments into the function
+        self.memory.append( memory(*args) )
+    def get_sample(self):
         return random.sample(self.memory,self.batch_size)
+    def batch_size(self):
+        return self.batch_size
     def __len__(self):
         return len(self.memory)
 
@@ -60,17 +61,19 @@ class Agent():
         self.action_size = 4
         self.qnet_local = DNNetwork().to(device)
         self.qnet_target = DNNetwork().to(device)
-        self.optimize = optim.SGD(self.qnet_local.parameters(), lr=5*10**-4)    # huber loss as alternative?
+        self.optimize = optim.Adam(self.qnet_local.parameters(), lr=1e-4)    # huber loss as alternative?
         self.memory = Replay_memory(100,10)
+        self.batch_size = self.memory.batch_size
         self.t_step = 0
 
     def step(self, *args):
-        self.memory.push(*args)
+        self.memory.remember(*args)
         self.t_step = (self.t_step + 1) % TARGET_UPDATE
-        if (self.t_step % TARGET_UPDATE == 0) and (self.memory == 100):
-            self.learn( self.memory.sample() )
+        if (self.t_step % TARGET_UPDATE == 0) and ( len(self.memory) >= self.batch_size):
+            self.learn( self.memory.get_sample() )
 
-    def act(self, state, eps=0.):       # copied from tutorial
+    def act(self, state, eps=0.):       # parameter eps for eps-greedy action selection
+        # convert the array from env into torch.tensor in float form
         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
         self.qnet_local.eval()
         with torch.no_grad():
@@ -79,7 +82,7 @@ class Agent():
         return np.argmax(action_values.cpu().data.numpy())
 
     def learn(self, exp, gamma=0.99):
-        s, a, r, next_s = exp
+        s, a, r, next_s = exp           # 10 memories to unpack
         q_target_next = self.qnet_target(next_s).detach().max(1)[0].unsqueeze(1)
 
         # Bellman equation. Calculating q_target and and current q_value
@@ -110,19 +113,19 @@ TARGET_UPDATE = 5
 
 
 ### Training
-def run_agent(runs=2000, gen_life=100):
+def run_agent(episodes=2000, play_time=5000):
     scores = []  # list containing scores from each episode
     scores_window = deque(maxlen=100)   # last 100 scores
-    for episode in range(runs):
-        state = env.reset()[0]    # if specific seed used, no improvement of the agent
+    for episode in range(episodes):
+        state = env.reset()[0]          # if specific seed used, no improvement of the agent
         score = 0
-        for i in range(gen_life):
-            action = agent.act(state)
-            next_state, reward, _, trunc, done = env.step(action)
-            agent.step(state, action, reward, next_state)
+        for time in range(play_time):   # define "playtime" of an agent in environment
+            action = agent.act(state)   # act on primary state, get best action from NN
+            next_state, reward, terminated, _, done = env.step(action)  # environment takes one step according to chosen the action
+            agent.step(state, action, reward, next_state)               # agent
             state = next_state
             score += reward
-            if trunc or done:
+            if terminated or done:
                 break
         scores_window.append(score)  # save most recent 100 scores
         scores.append(score)
--- TESTING_PyTorch.py
+++ TESTING_PyTorch.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as tfunc
 import gym
 
-### This file is for testing/learning pytorch functions and how to apply them correctly
+### This file is for testing/learning pytorch and gym functions and how to apply them correctly
 
 ### Loop testing whether a condition in the environment (both legs touching the ground // True) is met. Then break the loop and return total score
 # total_reward = 0.0
@@ -16,21 +17,14 @@ import gym
 #         break
 # print("Episode done in %d steps. Total score: %s" % (total_steps,total_reward))
 
-# class box():
-#     def __init__(self,dimensions):
-#         self.dim = dimensions
-#
-# k = box(24)
-# print(k.dim)
-#
 # m = nn.Softmax(dim=1)
 # input = torch.randn(2,3)
 # print(input, m(input), sep="\n")
 
 ### Environment of the Lunar Lander
-env = gym.make("LunarLander-v2")
-obs = env.reset(seed=0)
-print(env.step(0),"\nLength: ", len(env.step(0)))
+# env = gym.make("LunarLander-v2")
+# obs = env.reset(seed=0)
+# print(env.step(0),"\nLength: ", len(env.step(0)))
 # print(obs, "\n")
 # print(env.action_space)
 # action = env.action_space.sample()
@@ -46,3 +40,44 @@ print(env.step(0),"\nLength: ", len(env.step(0)))
 # x = torch.flatten(x)
 # x = torch.flatten(x)
 # print(x)
+
+### handling of PyTroch NN
+# N1 = nn.Linear(8, 32)
+# N2 = nn.Linear(32,4)
+# input = torch.from_numpy( gym.make('LunarLander-v2').reset()[0] )
+# print('State from reset environment:\n', input)
+# output = N1(input)
+# print('\nState run through NN:\n', output)
+# output = N2(output)
+# print(output)
+
+### torch.from_numpy() - convert a numpy array into a torch.tensor()
+
+# net = nn.Linear(6, 30)
+# net2 = nn.Linear(30,2)
+# T = torch.randn(2, 6)
+# T2 = torch.tensor([[1,2,3,4,5,6],[-1,2,-3,4,-5,6]])
+# print('\nTensor with ReLU applied:')
+# T = tfunc.relu(T)
+# T2 = tfunc.relu(T2)
+# print(T, '\n',T2)
+# print('\nTensor goes through NN:')
+# print( net(T) )
+# print( net2( net(T) ) )
+# # print( net(T2) )
+# print('\nReLU applied to NN:')
+# print( tfunc.relu( net(T) ) )
+
+##### Gym
+# env = gym.make("LunarLander-v2", render_mode="human")
+#
+# observation, info = env.reset(seed=0)
+# print(observation, "\n", info)
+#
+# for runs in range(200):
+#     observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+#     # print("Obs: %s\nr = %s\nTerm: %s\nTrunc: %s\nInfo: %s"%(observation,reward,terminated,truncated,info))
+#
+#     if terminated:
+#         observation, info = env.reset()
+#         env.close()
