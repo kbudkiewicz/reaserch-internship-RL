17c73c83b57554d959768cec5da8a4be2ca3667e
kbudkiewicz <krystianbudkiewicz@gmail.com>
2022-11-02 12:51:54 +0100

    02.11.22 Changing the moving average of plots from 100 to 10 values

--- Diagnostics/finalplot.py
+++ Diagnostics/finalplot.py
@@ -35,13 +35,13 @@ def lin_var_plot(matrices, labels, title='', legend_loc='lower right',smooth=Tru
         # smooths the plot with moving average
         if smooth == True:
             # redefine means as a moving average of last 100 values
-            moving_avg = deque(maxlen=100)
+            moving_avg = deque(maxlen=10)
             for i in range(matrix_width):
                 moving_avg.append(means[i])
                 means[i] = np.mean(moving_avg)
 
             # redefine standard deviation (sdevs) as moving average of last 100 values
-            moving_sdev = deque(maxlen=100)
+            moving_sdev = deque(maxlen=10)
             for i in range(matrix_width):
                 moving_sdev.append(sdevs[i])
                 sdevs[i] = np.mean(moving_sdev)
@@ -66,24 +66,24 @@ def lin_var_plot(matrices, labels, title='', legend_loc='lower right',smooth=Tru
 
 ### selective plotting
 # comparison untrained vs imported state_dict
-# labels = ('Untrained','Trained')
-# lin_var_plot( [d.M_n2_s, d.M_sd_s], labels, legend_loc='lower right')
-# lin_var_plot( [d.M_n2_l, d.M_sd_l], labels, legend_loc='upper right')
+labels = ('Untrained','Trained')
+lin_var_plot( [d.M_n2_s, d.M_sd_s], labels, legend_loc='lower right')
+lin_var_plot( [d.M_n2_l, d.M_sd_l], labels, legend_loc='upper right')
 
 # amount of hidden layers comparison
-# labels = ('3 hidden layers', '4 hidden layers', '5 hidden layers')
-# lin_var_plot( [d.M_n2_s, d.M_hl4_s, d.M_hl5_s], labels, title='Moving score average with different hidden layer sizes', legend_loc='lower right')
-# lin_var_plot( [d.M_n2_l, d.M_hl4_l, d.M_hl5_l], labels, title='Moving loss average with different hidden layer sizes', legend_loc='upper right')
+labels = ('3 hidden layers', '4 hidden layers', '5 hidden layers')
+lin_var_plot( [d.M_n2_s, d.M_hl4_s, d.M_hl5_s], labels, title='Moving score average with different hidden layer sizes', legend_loc='lower right')
+lin_var_plot( [d.M_n2_l, d.M_hl4_l, d.M_hl5_l], labels, title='Moving loss average with different hidden layer sizes', legend_loc='upper right')
 
 # amount of neurons comparison
-# labels = ('32 neurons', '64 neurons', '128 neurons', '256 neurons')
-# lin_var_plot([d.M_n1_s, d.M_n2_s, d.M_n3_s, d.M_n4_s], labels, title='Moving score average across different neuron amount', legend_loc='lower right')
-# lin_var_plot([d.M_n1_l, d.M_n2_l, d.M_n3_l, d.M_n4_l], labels, title='Moving loss average across different neuron amount', legend_loc='upper right')
+labels = ('32 neurons', '64 neurons', '128 neurons', '256 neurons')
+lin_var_plot([d.M_n1_s, d.M_n2_s, d.M_n3_s, d.M_n4_s], labels, title='Moving score average across different neuron amount', legend_loc='lower right')
+lin_var_plot([d.M_n1_l, d.M_n2_l, d.M_n3_l, d.M_n4_l], labels, title='Moving loss average across different neuron amount', legend_loc='upper right')
 
 # epsilon comparison
-# labels = ('eps_decay = 0.995','eps_decay = 0.9975','eps_decay = 0.999')
-# lin_var_plot([d.M_e995_s, d.M_e9975_s, d.M_e999_s], labels, legend_loc='lower right')
-# lin_var_plot([d.M_e995_l, d.M_e9975_l, d.M_e999_l], labels, legend_loc='upper right')
+labels = ('eps_decay = 0.995','eps_decay = 0.9975','eps_decay = 0.999')
+lin_var_plot([d.M_e995_s, d.M_e9975_s, d.M_e999_s], labels, legend_loc='lower right')
+lin_var_plot([d.M_e995_l, d.M_e9975_l, d.M_e999_l], labels, legend_loc='upper right')
 
 # learning rate comparison
 labels = ('lr = 1e-4','lr = 2.5e-4', 'lr = 5e-4')
