a2ff5db2d473c88e91389038d1ece3bc5e72bd41
kbudkiewicz <krystianbudkiewicz@gmail.com>
2024-08-18 21:40:39 +0200

    initial commit

--- agent.py
+++ agent.py
@@ -6,55 +6,58 @@ import torch.nn.functional as F
 import torch.optim as optim
 import gym
 from collections import deque, namedtuple
-from Diagnostics.diagnostics import diagnose
+from diagnostics.diagnostics import diagnose
 
 ### Assigning the device
-if torch.cuda.is_available():
-    device = "cuda"
-else:
-    device = "cpu"
+device='cuda' if torch.cuda.is_available() else 'cpu'
 print("Current device: %s \n" % device.upper())
 
 ### Creating gym' Lunar Lander environment
 env = gym.make("LunarLander-v2", render_mode='human')    # render_mode='human'
 
+
 class DNNetwork(nn.Module):
-    def __init__(self,layer_size=64):                   # CNN not needed for research internship -> Linear layers, batchnormalisation not needed
-        super(DNNetwork,self).__init__()                # super(superclass) - inherit the methods of the superclass (class above this one). Here: inherit all __init__ method of DQN
+    def __init__(self, layer_size=64):                   # CNN not needed for research internship -> Linear layers, batchnormalisation not needed
+        super(DNNetwork, self).__init__()                # super(superclass) - inherit the methods of the superclass (class above this one). Here: inherit all __init__ method of DQN
         self.layer_size = layer_size
-        self.lin1 = nn.Linear(8,layer_size)             # input (here 8) corresponds to the size of observation space
-        self.lin2 = nn.Linear(layer_size,layer_size)    # layer_size = amount of neurons between hidden layers
-        self.lin3 = nn.Linear(layer_size,layer_size)
-        self.lin4 = nn.Linear(layer_size,4)             # output (here 4) corresponds to the size of action space
+        self.lin1 = nn.Linear(8, layer_size)             # input (here 8) corresponds to the size of observation space
+        self.lin2 = nn.Linear(layer_size, layer_size)    # layer_size = amount of neurons between hidden layers
+        self.lin3 = nn.Linear(layer_size, layer_size)
+        self.lin4 = nn.Linear(layer_size, 4)             # output (here 4) corresponds to the size of action space
         self.to(device)
 
         ### For CNNs
         # stide         gives how much the filter is moved across the matrix (e.g. stride = 2 means: move the filter 2 indicies to the right of the matrix)
         # kernel_size   size of the tensor/matrix filter between convolutional layers
 
-    def forward(self,state):
-        x = F.relu( self.lin1(state) )               # ReLU - rectified linear unit. take max(0,input) of the input
-        x = F.relu( self.lin2(x) )
-        x = F.relu( self.lin3(x) )
+    def forward(self, state):
+        x = F.relu(self.lin1(state))               # ReLU - rectified linear unit. take max(0,input) of the input
+        x = F.relu(self.lin2(x))
+        x = F.relu(self.lin3(x))
         action_set = self.lin4(x)
         return action_set
 
 ### Defining replay memory
-memory = namedtuple( 'Memory', ('s','a','r','next_s','term') )
+memory = namedtuple( 'Memory', ('s', 'a', 'r', 'next_s', 'term') )
 
-class Replay_memory(object):
-    def __init__(self,memory_size, batch_size):
+
+class ReplayMemory(object):
+    def __init__(self, memory_size, batch_size):
         self.memory = deque(maxlen=memory_size)
         self.batch_size = batch_size
+
     def remember(self, *args):              # *args: put any amount of arguments into the function
-        self.memory.append( memory(*args) )
+        self.memory.append(memory(*args))
+
     def get_sample(self):
-        return random.sample(self.memory,self.batch_size)
+        return random.sample(self.memory, self.batch_size)
+
     def __len__(self):
         return len(self.memory)
 
+
 class Agent:
-    def __init__(self, memory_size, batch_size, tau, gamma, epsilon, learning_rate):
+    def __init__(self, memory_size, batch_size, tau, gamma, epsilon, learning_rate, net_update):
         self.state_size = 8
         self.action_size = 4
         self.memory_size = memory_size
@@ -64,10 +67,11 @@ class Agent:
         self.eps = epsilon
         self.lr = learning_rate
         self.loss = 300
+        self.net_update = net_update
         self.qnet_local = DNNetwork().to(device)
         self.qnet_target = DNNetwork().to(device).eval()
         self.optimizer = optim.Adam(self.qnet_local.parameters(), self.lr)    # huber loss as alternative?
-        self.memory = Replay_memory(memory_size,batch_size)
+        self.memory = ReplayMemory(memory_size, batch_size)
         self.t_step = 0
 
         self.list_size = [i for i in range(self.batch_size)]
@@ -85,15 +89,15 @@ class Agent:
 
         # eps-greedy action selection
         if random.random() > self.eps:
-            return np.argmax( action_values.detach().numpy() )
+            return np.argmax(action_values.detach().numpy())
         else:
-            return random.randint(0,3)
+            return random.randint(0, 3)
 
     def evaluate(self, *args):
         self.memory.remember(*args)     # input: s, a, r, next_s, terminated
         self.t_step = self.t_step + 1
-        if (self.t_step % NET_UPDATE == 0) and ( self.memory.__len__() >= self.batch_size ):
-            self.learn( self.memory.get_sample() )
+        if (self.t_step % self.net_update == 0) and (self.memory.__len__() >= self.batch_size):
+            self.learn(self.memory.get_sample())
 
     def learn(self, exp):
         # unpack memories into a tensor/vector with states, actions, or rewards. attach an argument of named tuple from each memory
@@ -125,27 +129,26 @@ def calc_eps(current_episode, eps_start, eps_end, eps_term):
     eps = slope * current_episode + eps_start
     return eps
 
-### Training
-def run_agent(episodes=1500, play_time=1000):
+# Training
+def run_agent(hparams, episodes: int=1500, play_time: int=1000):
+    '''
+    Initializes the agents' training loop.
+    :param hparams:     [dict] containing hyperparameters
+    :param episodes:    [int] maximum number of training episodes
+    :param play_time:   [int] maximum number of actions per episode
+    :return: scores, loss, last_episode
+    '''
     # print statement returns currently used variables
-    print(  '| Variables during this run |\n'+ 60*'-' +
-            '\n%s\t\t# of Episodes\n'
-            '%s\t\tPlay time\n'
-            '%s\t\t\tNN\' hidden layer size\n'
-            '%s\t\t\tNetwork update\n'
-            '%s\t\tAgents memory size\n'
-            '%s\t\t\tMemory batch size\n'
-            '%s\t\tTau\n'
-            '%s\t\tGamma\n'
-            '%s\t\tLearning rate\n'
-            '%s\t\t\tEpsilon start\n'
-            '%s\t\tEpsilon end\n'
-            '%s\t\tEpsilon termination\n'
-            % (episodes,play_time,LAYER_SIZE,NET_UPDATE,MEMORY_SIZE,BATCH_SIZE,TAU,GAMMA,LR,EPS_START,EPS_END,EPS_TERM)
-            + 60*'-' )
+    print('| Variables during this run |')
+    print(f'\n\tEpisodes: {episodes}')
+    print(f'\n\tPlay time: {play_time}')
+    for k, v in hparams.items():
+        print(f'\n\t{k.upper()}: {v}')
 
+    eps_start, eps_end, eps_term = hparams['eps_start'], hparams['eps_end'], hparams['eps_term']
     scores, loss = [], []
     last_scores, last_loss = deque(maxlen=100), deque(maxlen=100)
+
     for episode in range(episodes):
         state = env.reset()[0]
         score = 0
@@ -157,7 +160,7 @@ def run_agent(episodes=1500, play_time=1000):
             score += reward
             if terminated or truncated:
                 break
-        agent.eps = max( EPS_END, calc_eps(episode,EPS_START,EPS_END,EPS_TERM) )         # recalculate current eps linearly
+        agent.eps = max(eps_end, calc_eps(episode, eps_start, eps_end, eps_term))         # recalculate current eps linearly
 
         scores.append(score)
         loss.append( int(agent.loss) )
@@ -167,7 +170,7 @@ def run_agent(episodes=1500, play_time=1000):
         if episode % 50 == 0:
             print( "Running episode %s. Currently averaged score: %.2f" % (episode, np.mean(last_scores)) )
             print( 'Loss average = %s' % np.mean(last_loss) )
-            if agent.eps > EPS_END:
+            if agent.eps > eps_end:
                 print('Eps = %s' % agent.eps)
 
         if np.mean(last_scores) >= 200.0:
@@ -180,26 +183,33 @@ def run_agent(episodes=1500, play_time=1000):
     return scores, loss, episode
 
 ### Training parameters
-GAMMA = 0.99
-TAU = 2.5e-3
-NET_UPDATE = 6
-LAYER_SIZE = 64
-MEMORY_SIZE = 100000
-BATCH_SIZE = 100
-LR = 1e-3
-EPS_START = 0.7
-EPS_END = 0.05
-EPS_TERM = 1000        # value at which EPS_END will be achieved
+hparams = {'gamma': 0.99,
+           'tau': 2.5e-3,
+           'net_update': 6,
+           'lr': 1e-3,
+           'batch_size': 100,
+           'memory_size': 100_000,
+           'layer_size': 100,
+           'eps_start': 0.7,
+           'eps_end': 0.05,
+           'eps_term': 1e3}    # episode No at which EPS_END will be achieved
 
 ### importing of state dictionary of already trained agent
-EPS_START, EPS_END = 0, 0
-agent = Agent(memory_size=MEMORY_SIZE, batch_size=BATCH_SIZE, gamma=GAMMA, tau=TAU, learning_rate=LR, epsilon=EPS_START)
+eps_start, eps_end = 0, 0
+agent = Agent(memory_size=hparams['memory_size'],
+              batch_size=hparams['batch_size'],
+              gamma=hparams['gamma'],
+              tau=hparams['tau'],
+              learning_rate=hparams['lr'],
+              net_update=hparams['net_update'],
+              epsilon=eps_start)
+
 agent.qnet_local.load_state_dict( torch.load('C:/Users/kryst/Documents/GitHub/research-internship-RL/Diagnostics/Exponential epsilon decay/Using state dictionary from previous runs/state_dict.pt') )
 agent.qnet_local.eval()
 agent.qnet_target.load_state_dict( torch.load('C:/Users/kryst/Documents/GitHub/research-internship-RL/Diagnostics/Exponential epsilon decay/Using state dictionary from previous runs/state_dict.pt'))
 # if network variables are named differently in the dictionary and code then an error occurs -> same variable names so import can work properly
 
-scores, loss, last_episode = run_agent()
+scores, loss, last_episode = run_agent(hparams)
 print(scores)
 print(loss)
 # diagnose(scores, loss, last_episode)
