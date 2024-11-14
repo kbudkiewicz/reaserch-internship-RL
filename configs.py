9d8cc69b168053fa98a1249ba18f3dff90150f2f
kbudkiewicz <krystianbudkiewicz@gmail.com>
2024-11-14 13:52:25 +0100

    update configs and training loop

--- configs.py
+++ configs.py
@@ -1,16 +1,19 @@
 class AgentConfig:
+    '''
+    Hyperparameters for the RL agent. Contains both agent, as well as network hyperparameters.
+    '''
     state_space: int = 8
     action_space: int = 4
     memory_size: int = 100_000
-    episodes: int = 1_000
+    episodes: int = 1_500
     play_time: int = 1_000
     loss: float = 0
     t_step: int = 0
     batch_size: int = 64
-    tau: float = 0.01
+    tau: float = 2.5e-3
     gamma: float = 0.99
-    eps: float = 1.0
-    eps_start: float = 0.01
-    eps_end: float = 0.99
-    lr: float = 0.001
+    lr: float = 1e-3
     net_update_freq: int = 6
+    eps_start: float = 0.7
+    eps_end: float = 0.05
+    eps_term: int = 500
--- training.py
+++ training.py
@@ -1,31 +1,18 @@
 from agent import *
+from configs import AgentConfig
 
-# Assigning the device
+# assign the device and initialize the agent
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Current device: {device.upper()}')
+agent = Agent(AgentConfig)
 
-# initializing gym's Lunar Lander environment
+# initializing the Lunar Lander environment
 env = gym.make("LunarLander-v2")
-
-# training
-# initialize the training hyperparameters
-hparams = {'gamma': 0.99,
-           'tau': 2.5e-3,
-           'net_update': 6,
-           'lr': 1e-3,
-           'batch_size': 100,
-           'memory_size': 100_000,
-           'eps_start': 0.7,
-           'eps_end': 0.05,
-           'eps_term': 500}    # episode No at which EPS_END will be achieved
 
 # visualize: uncomment code below to run the best agent trained so far
 # render_mode='human' for visual render after the training is done
 # hparams['eps_start'] = 0
-env = gym.make("LunarLander-v2")#, render_mode='human')
-
-# initialize the agent
-agent = Agent(hparams=hparams)
+# env = gym.make("LunarLander-v2", render_mode='human')
 
 # WARNING: if network variables are named differently in the dictionary and code then an error occurs
 # -> same variable names so import can work properly
@@ -33,6 +20,6 @@ agent = Agent(hparams=hparams)
 # agent.qnet_local.eval()
 # agent.qnet_target.load_state_dict(torch.load('diagnostics/state_dicts/state_dict_.pt'))
 
-scores, loss, last_episode = agent.train(hparams, environment=env)
+scores, loss, last_episode = agent.train(environment=env)
 print(f'Scores: {scores}\n'
       f'Loss: {loss}\n')
